8ff4c534a6b6eb7cdcfb54e85f5eea220cb788ce
20200197 <84478171+20200197@users.noreply.github.com>
2022-04-02 08:37:47 -0600

    Pagina inicio

---
+++ DeliverySystem/controllers/activation.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+     M.Sidenav.init(document.querySelectorAll('.sidenav'));
+     M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+   
+   });
+
+  
