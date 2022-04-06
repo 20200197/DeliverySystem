b51fbb291fb4865993d3ad1f0dc68adf4bbb53c4
20200197 <84478171+20200197@users.noreply.github.com>
2022-04-06 16:29:27 -0600

    privado y publico: adminitrar y mis productos

--- DeliverySystem/controllers/private/admin_client.js
+++ DeliverySystem/controllers/private/admin_client.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
      M.Sidenav.init(document.querySelectorAll('.sidenav'));
      M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+     M.Materialbox.init(document.querySelectorAll('.materialboxed'));
      
    });
 
@@ -8,3 +9,4 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
 });
+
