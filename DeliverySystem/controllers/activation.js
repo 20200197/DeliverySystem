d6e9ca2ab796315d8f762807bbfa6b2675794868
RodriB17 <79107339+RodriB17@users.noreply.github.com>
2022-04-07 13:29:01 -0600

    Add files via upload

--- DeliverySystem/controllers/activation.js
+++ DeliverySystem/controllers/activation.js
@@ -4,4 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
    
    });
 
-  
+   document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
+
+  $(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
+
+  $('#textarea1').val('New Text');
+  M.textareaAutoResize($('#textarea1'));
