061684dbb67bf22f1de24e1ba1c63ec16ece1f24
RoiHdz <rodrigophernandez@hotmail.com>
2022-04-08 07:38:13 -0600

    Union de sitio publico y privado

---
+++ DeliverySystem/controladores/privado/activation.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+     M.Sidenav.init(document.querySelectorAll('.sidenav'));
+     M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+     M.Materialbox.init(document.querySelectorAll('.materialboxed'));
+   });
+
+   document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
