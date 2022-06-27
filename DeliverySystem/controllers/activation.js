0eb85b75f532f83b89d3b5a6af798b3bb9a29777
ErnestoG12 <85888151+ErnestoG12@users.noreply.github.com>
2022-06-27 16:50:53 -0600

    arreglar carpetas

--- DeliverySystem/controllers/activation.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-     M.Sidenav.init(document.querySelectorAll('.sidenav'));
-     M.Tooltip.init(document.querySelectorAll('.tooltipped'));
-   
-   });
-
-  
