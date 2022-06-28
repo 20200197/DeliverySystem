85c9fc43c783e82007b8f55418a42793dd50387a
Giovanni Arias <85887831+20200044@users.noreply.github.com>
2022-06-27 21:53:13 -0600

    eliminando archivo antiguo de controllers

--- DeliverySystem/controllers/activation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-     M.Sidenav.init(document.querySelectorAll('.sidenav'));
-     M.Tooltip.init(document.querySelectorAll('.tooltipped'));
-     M.Modal.init(document.querySelectorAll('.modal'));
-     M.FormSelect.init(document.querySelectorAll('select'));
-   });
-
-  
