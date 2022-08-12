46f8bf8fb5817ddde981ec0689f8afaaefb0bce2
10Oliver <oliver.erazo1@gmail.com>
2022-08-12 09:37:47 -0600

    Mantemiento read en perfil terminado

---
+++ api/modelos/perfil_cliente.php
@@ -0,0 +1,111 @@
+<?php
+/*
+*	Clase para manejar la tabla usuarios de la base de datos.
+*   Es clase hija de Validator.
+*/
+class PerfilCliente extends Validator
+{
+    //Se delacara las variables globales
+    private $identificador = null;
+    private $nombreCliente = null;
+    private $apellidoCliente = null;
+    private $correoCliente = null;
+    private $telefonoCliente = null;
+    private $fotoCliente = null;
+    private $passCliente = null;
+    private $ruta = '../imagenes/cliente/foto/';
+
+    //Se crean los constructores para colocar los datos
+    public function setIdentificador($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->identificador = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setNombreCliente($value)
+    {
+        if ($this->validateAlphabetic($value, 2, 30)) {
+            $this->nombreCliente = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setApellidoCliente($value)
+    {
+        if ($this->validateAlphabetic($value, 2, 30)) {
+            $this->apellidoCliente = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setCorreoCliente($value)
+    {
+        if ($this->validateEmail($value)) {
+            $this->correoCliente = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setTelefonoCliente($value)
+    {
+        if ($this->validatePhone($value)) {
+            $this->telefonoCliente = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setPassCliente($value)
+    {
+        if ($this->validatePassword($value)) {
+            $this->passCliente = password_hash($value, PASSWORD_DEFAULT);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setFotoCliente($value)
+    {
+        if ($this->validateImageFile($value, 2000, 2000)) {
+            $this->fotoCliente = $this->getFileName();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Métodos para obtener el valor de las variables
+
+    public function getRuta()
+    {
+        return $this->ruta;
+    }
+
+    public function getFotoCliente()
+    {
+        return $this->fotoCliente;
+    }
+
+    //Funciones para realizar los mantenimientos en la DB
+
+    //Obtener los datos del perfil
+    public function datosPerfil()
+    {
+        $sql = "SELECT nombre_cliente, apellido_cliente, correo_cliente, telefono_cliente, foto_cliente
+        FROM cliente WHERE id_cliente = ?";
+        $params = array($this->identificador);
+        return Database::getRow($sql, $params);
+    }
+}
--- controladores/publico/publico_cliente/perfil.js
+++ controladores/publico/publico_cliente/perfil.js
@@ -1,13 +1,24 @@
 //Se crea la constante para la API
-const API_PERFIL = SERVER + 'publico/perfil.php?action=';
+const API_PERFIL = SERVER + 'publico/perfil_cliente.php?action=';
 
 //Se crea el método que se ejecutará cuando se ejecuta la página
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     //Se cargan los datos
+    readRows(API_PERFIL);
 });
 
+//Se crea una función para cargar los datos en la página
+function fillTable(datos) {
+    //Se empiezan a cargar los datos obtenidos de la API
+    document.getElementById('nombre').value = datos.nombre_cliente;
+    document.getElementById('apellido').value = datos.apellido_cliente;
+    document.getElementById('correo').value = datos.correo_cliente;
+    document.getElementById('telefono').value = datos.telefono_cliente;
+    document.getElementById('fotoPerfil').src = SERVER + '/imagenes/cliente/foto/' + datos.foto_cliente;
+}
+
 //Se crea el método que verificará la autentidad del usuario
-document.getElementById('confirmacionAutenticidad').addEventListener("submit", function (event) { 
+document.getElementById('confirmacionAutenticidad').addEventListener("submit", function(event) {
     //Se previene la recarga de la página
     event.preventDefault();
     //Comprueba la contraseña
@@ -17,4 +28,4 @@ document.getElementById('confirmacionAutenticidad').addEventListener("submit", f
     document.getElementById('confirmacionAutenticidad').reset();
     //Se abre el formulario
     M.Modal.getInstance(document.getElementById("datos-cuenta")).open();
-})
+});
