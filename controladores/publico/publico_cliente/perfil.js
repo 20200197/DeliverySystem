aafe951975384cbe58f6bfb347902968a84b8ee4
10Oliver <oliver.erazo1@gmail.com>
2022-08-12 11:04:21 -0600

    Mejorando diseños enteractivos

--- controladores/publico/publico_cliente/perfil.js
+++ controladores/publico/publico_cliente/perfil.js
@@ -3,6 +3,10 @@ const API_PERFIL = SERVER + 'publico/perfil_cliente.php?action=';
 
 //Se crea el método que se ejecutará cuando se ejecuta la página
 document.addEventListener('DOMContentLoaded', function() {
+    //Se esconden los botones de modificación
+    document.getElementById('guardar').style.display = "none";
+    document.getElementById('cancelar').style.display = "none";
+    document.getElementById('botonFoto').setAttribute('disabled', true);
     //Se cargan los datos
     readRows(API_PERFIL);
 });
@@ -29,3 +33,94 @@ document.getElementById('confirmacionAutenticidad').addEventListener("submit", f
     //Se abre el formulario
     M.Modal.getInstance(document.getElementById("datos-cuenta")).open();
 });
+
+
+//Función para previsualizar la foto de perfil
+function leerImg(input, img_destino) {
+    //Se obtiene los archivos del input
+    let archivos = input.files;
+    //Se verifica si está vacío
+    if (!archivos || !archivos.length) {
+        img_destino.src = "../../recursos/img/publico/sin.png";
+        return;
+    }
+    //Carga la imagen
+    const visualizar = archivos[0];
+    const url = URL.createObjectURL(visualizar);
+    img_destino.src = url;
+}
+
+//Función para activar el cambio de datos
+function activar() {
+    if (document.getElementById('activar').style.display == "none") {
+        //Se escoden los botones
+        document.getElementById('guardar').style.display = "none";
+        document.getElementById('cancelar').style.display = "none";
+        //Se muestra el botón
+        document.getElementById('activar').style.display = "";
+        //Se desactivan los campos de modificación
+        document.getElementById('nombre').disabled = true;
+        document.getElementById('apellido').disabled = true;
+        document.getElementById('correo').disabled = true;
+        document.getElementById('telefono').disabled = true;
+        document.getElementById('nombre').style = 'border-bottom: 1px dotted rgba(0, 0, 0, 0.42); box-shadow: none;';
+        document.getElementById('apellido').style = 'border-bottom: 1px dotted rgba(0, 0, 0, 0.42); box-shadow: none;';
+        document.getElementById('correo').style = 'border-bottom: 1px dotted rgba(0, 0, 0, 0.42); box-shadow: none;';
+        document.getElementById('telefono').style = 'border-bottom: 1px dotted rgba(0, 0, 0, 0.42); box-shadow: none;';
+        document.getElementById('botonFoto').setAttribute('disabled', true);
+        //Se rellenan los campos con los datos actuales
+        readRows(API_PERFIL);
+    } else {
+        //Se muestran los botones
+        document.getElementById('guardar').style.display = "";
+        document.getElementById('cancelar').style.display = "";
+        //Se esconde el botón
+        document.getElementById('activar').style.display = "none";
+        //Se activan los campos de modificación
+        document.getElementById('nombre').disabled = false;
+        document.getElementById('apellido').disabled = false;
+        document.getElementById('correo').disabled = false;
+        document.getElementById('telefono').disabled = false;
+        document.getElementById('botonFoto').removeAttribute('disabled');
+
+
+    }
+
+}
+
+//Función para guardar los cambios del usuario
+function guardarDatos() {
+    //Se realiza una petición para validar que si se han realizado cambios a guardar
+    fetch(API_PERFIL + 'readAll', {
+        method: 'get',
+    }).then(function(request) {
+        //Se verifica si se ha ejecutado la petición
+        if (request.ok) {
+            //Se pasa a formato JSOn
+            request.json().then(function(response) {
+                //Se revisa el estado devuelto por la API
+                if (response.status) {
+                    //Se revisan los datos para validar si han sido modificados
+                    imagen = document.getElementById('fotoPerfil').src.split('/');
+                    if (
+                        imagen[imagen.length - 1] == response.dataset.foto_cliente ||
+                        document.getElementById('nombre').value == response.dataset.nombre_cliente ||
+                        document.getElementById('apellido').value == response.dataset.apellido_cliente ||
+                        document.getElementById('correo').value == response.dataset.correo_cliente ||
+                        document.getElementById('telefono').value == response.dataset.telefono_cliente
+                    ) {
+                        sweetAlert(3, 'Debes modificar datos para poder actualizar', null);
+                    } else {
+                        sweetAlert(1, 'Actualizando', null);
+                    }
+
+
+                } else {
+
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    });
+}
