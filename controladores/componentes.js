239a772f5b014f25b9422f04be37029b4d759f49
10Oliver <oliver.erazo@gmail.com>
2022-08-18 11:31:14 -0600

    Prueba de ejemplo para gráfica de linea

--- controladores/componentes.js
+++ controladores/componentes.js
@@ -474,6 +474,156 @@ function semiPastel(CLASS, titulos, datos) {
     });
 }
 
+
+function lineaI(CLASS, cabeceras, datos) {
+    new Chartist.Line(CLASS, {
+        labels: cabeceras,
+        series: datos
+    }, {
+        fullWidth: true,
+        chartPadding: {
+            right: 40
+        },
+        plugins: [
+            Chartist.plugins.tooltip()
+        ]
+    });
+
+}
+
+//Función para crear una gráfica líneal con puntos interpolados
+function lineaI2(CLASS, cabeceras, datos) {
+    //Se crea la gráfica a base de la clase y los datos
+    var chart = new Chartist.Line(
+        CLASS, {
+            labels: cabeceras,
+            series: datos,
+        }, {
+            fullWidth: true,
+            chartPadding: {
+                right: 40
+            },
+            plugins: [
+                Chartist.plugins.tooltip()
+            ]
+        }
+    );
+    /* --Código para que se encuentre animado---- */
+    var seq = 0,
+        delays = 100,
+        durations = 500;
+
+    //Se reinicia la animación
+    chart.on("created", function() {
+        seq = 0;
+    });
+
+    //Se dibuja los elementos usando la API de Chartist
+    chart.on("draw", function(data) {
+        seq++;
+
+        if (data.type === "line") {
+            //Se especifican la opacidad y las animacion de las lineas usando ccs3
+            data.element.animate({
+                opacity: {
+                    //Se especifica el delay antes de seguir o cambiar de fase
+                    begin: seq * delays + 500,
+                    //Duración de la animación
+                    dur: durations,
+                    //El valor donde se iniciará la animación
+                    from: 0,
+                    //El valor donde finalizará la animación
+                    to: 1,
+                },
+            });
+        } else if (data.type === "label" && data.axis === "x") {
+            data.element.animate({
+                y: {
+                    begin: seq * delays,
+                    dur: durations,
+                    from: data.y + 100,
+                    to: data.y,
+                    easing: "easeOutQuart",
+                },
+            });
+        } else if (data.type === "label" && data.axis === "y") {
+            data.element.animate({
+                x: {
+                    begin: seq * delays,
+                    dur: durations,
+                    from: data.x - 100,
+                    to: data.x,
+                    easing: "easeOutQuart",
+                },
+            });
+        } else if (data.type === "point") {
+            data.element.animate({
+                x1: {
+                    begin: seq * delays,
+                    dur: durations,
+                    from: data.x - 10,
+                    to: data.x,
+                    easing: "easeOutQuart",
+                },
+                x2: {
+                    begin: seq * delays,
+                    dur: durations,
+                    from: data.x - 10,
+                    to: data.x,
+                    easing: "easeOutQuart",
+                },
+                opacity: {
+                    begin: seq * delays,
+                    dur: durations,
+                    from: 0,
+                    to: 1,
+                    easing: "easeOutQuart",
+                },
+            });
+        } else if (data.type === "grid") {
+            //Animaciones para las líneas
+            var pos1Animation = {
+                begin: seq * delays,
+                dur: durations,
+                from: data[data.axis.units.pos + "1"] - 30,
+                to: data[data.axis.units.pos + "1"],
+                easing: "easeOutQuart",
+            };
+
+            var pos2Animation = {
+                begin: seq * delays,
+                dur: durations,
+                from: data[data.axis.units.pos + "2"] - 100,
+                to: data[data.axis.units.pos + "2"],
+                easing: "easeOutQuart",
+            };
+
+            var animations = {};
+            animations[data.axis.units.pos + "1"] = pos1Animation;
+            animations[data.axis.units.pos + "2"] = pos2Animation;
+            animations["opacity"] = {
+                begin: seq * delays,
+                dur: durations,
+                from: 0,
+                to: 1,
+                easing: "easeOutQuart",
+            };
+
+            data.element.animate(animations);
+        }
+    });
+
+    //Reanimación de la gráfica
+    chart.on("created", function() {
+        if (window.__exampleAnimateTimeout) {
+            clearTimeout(window.__exampleAnimateTimeout);
+            window.__exampleAnimateTimeout = null;
+        }
+        window.__exampleAnimateTimeout = setTimeout(chart.update.bind(chart), 18000);
+    });
+}
+
+
 // Función para mostrar un mensaje de confirmación al momento de cerrar sesión.
 function logOut(type) {
     Swal.fire({
--- graficas/graficas.js
+++ graficas/graficas.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }]
     };
     generarBarra(datos);
+    generarLinea(datos);
 });
 
 
@@ -61,4 +62,32 @@ function generarBarra(dataset) {
     //general.push(fila3);
     //Se manda a llamar la función para generar una gráfica
     barras(".barras", cabeceras, general);
+}
+
+function generarLinea(dataset) {
+    //Pasos anteriores del fetch
+    //Se crean los vectores generales donde se guardarán los datos
+    let cabeceras = []; //Vector donde se guardarán los titulos de la gráfica
+    let general = []; //Vector donde se guardarán los datos por toda la gráfica (Es un contenedor nada más)
+    //Se se desean agregar más lineas de la gráfica se deben de crear un vector más
+    let fila = []; //Vector donde se guardarán los datos por cada titulo de la gráfica (1 Línea)
+    let fila2 = []; //Vector donde se guardarán los datos por el mismo titulo de la gráfica (1 Línea)
+    let fila3 = []; //Vector donde se guardarán los datos por el mismo titulo de la gráfica (1 Línea)
+
+    //Se explorar fila por fila
+    dataset.dataset.map(function(row) {
+        //Se llenan los datos en los vectores generales
+        cabeceras.push(row.producto); //Se agrega un titulo
+        fila.push(row.cantidad); //Se agrega un dato para el titulo
+        //fila2.push(row.cantidad); //Si se desea agregar más de un datos por titulo se agregan aquí
+        //fila3.push(row.cantidad); //Si se desea agregar más de un datos por titulo se agregan aquí
+
+    });
+    //Se cargan los datos dentro del contenedor
+    general.push(fila);
+    //Si hay más lineas se agregan aquí también
+    //general.push(fila2);
+    //general.push(fila3);
+    //Se manda a llamar la función para generar una gráfica
+    lineaI(".linea", cabeceras, general);
 }
