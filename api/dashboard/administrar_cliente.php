15f274c4d2abdfc271880a0f173b2e9a7edd9787
Giovanni Arias <85887831+20200044@users.noreply.github.com>
2022-08-17 21:26:44 -0600

    api de pedidos cliente

--- api/dashboard/administrar_cliente.php
+++ api/dashboard/administrar_cliente.php
@@ -12,15 +12,42 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'exception' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['id_usuario'])) {
+    if (isset($_SESSION['id_admin'])) {
+        $result['session'] = 1;
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
-            case 'getData':
-                if($result['dataset'] = $administrar_cliente->obtenerClientes())
-                {
+            case 'readAll':
+                if ($result['dataset'] = $administrar_cliente->obtenerClientes()) {
                     $result['status'] = 1;
-                }//elseif($result['exception'] = Database)
-            //default:
+                } elseif ($result['exception'] = Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay datos de momento';
+                }
+                break;
+            case 'cambiarEstado':
+                if (!$administrar_cliente->setEstado($_POST['estado'])) {
+                    $result['exception'] = 'Estado no valido';
+                } elseif (!$administrar_cliente->setIdentificador($_POST['identificador'])) {
+                    $result['exception'] = 'Identificador no valido';
+                } elseif ($administrar_cliente->cambiarEstado()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Cambio de estado exitoso';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'buscar':
+                $_POST = $administrar_cliente->validateForm($_POST);
+                if (!$administrar_cliente->setBuscador($_POST['buscador'])) {
+                    $result['exception'] = 'Entrada de búsqueda no válida';
+                } elseif ($result['dataset'] = $administrar_cliente->buscar()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            default:
                 $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
         // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
--- api/dashboard/administrar_distribuidor.php
+++ api/dashboard/administrar_distribuidor.php
@@ -1,14 +1,14 @@
 <?php
 require_once('../ayudantes/database.php');
 require_once('../ayudantes/validator.php');
-require_once('../modelos/administrar_productos.php');
+require_once('../modelos/administrar_distribuidor.php');
 
 // Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
 if (isset($_GET['action'])) {
     // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
     session_start();
     // Se instancia la clase correspondiente.
-    $producto = new Producto;
+    $administrar_distribuidor = new Distribuidor;
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'exception' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
@@ -17,7 +17,7 @@ if (isset($_GET['action'])) {
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
             case 'readAll':
-                if ($result['dataset'] = $producto->readAll()) {
+                if ($result['dataset'] = $administrar_distribuidor->readAll()) {
                     $result['status'] = 1;
                 } elseif (Database::getException()) {
                     $result['exception'] = Database::getException();
@@ -25,55 +25,63 @@ if (isset($_GET['action'])) {
                     $result['exception'] = 'No hay datos registrados';
                 }
                 break;
-                //Buscar empleado
-            case 'search':
-                $_POST = $producto->validateForm($_POST);
-                if ($_POST['search'] == '') {
-                    $result['exception'] = 'Ingrese un valor para buscar';
-                    // $entrega->readAll();
-                } elseif ($result['dataset'] = $producto->searchRows($_POST['search'])) {
+                //Buscar dustribuidor
+                case 'search':
+                $_POST = $administrar_distribuidor->validateForm($_POST);
+                if ($result['dataset'] = $administrar_distribuidor->searchRows($_POST['data'])) {
                     $result['status'] = 1;
                     $result['message'] = 'Valor encontrado';
-                } elseif (Database::getException()) {
+                    } elseif (Database::getException()) {
                     $result['exception'] = Database::getException();
-                } else {
+                    } else {
                     $result['exception'] = 'No hay coincidencias';
+                    }
+                    break; 
+                //Crear fila
+                case 'create':
+                $_POST = $administrar_marca->validateForm($_POST);
+                if (!$administrar_marca->setNombreMarca($_POST['nombre_marca'])) {
+                    $result['exception'] = 'Marca incorrecta';
+                } elseif ($administrar_marca->createRow()) {
+                    $result['status']=1;
+                    $result['message'] = 'Marca registrada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
                 }
                 break;
-            case 'readOne':
-                if (!$producto->setId($_POST['idP'])) {
-                    $result['exception'] = 'Producto incorrecto';
-                } elseif ($result['dataset'] = $producto->readOne()) {
+                case 'readOne':
+                if (!$administrar_distribuidor->setId($_POST['id'])) {
+                    $result['exception'] = 'distribuidor incorrecto';
+                } elseif ($result['dataset'] = $administrar_distribuidor->readOne()) {
                     $result['status'] = 1;
                 } elseif (Database::getException()) {
                     $result['exception'] = Database::getException();
                 } else {
-                    $result['exception'] = 'Producto inexistente';
+                    $result['exception'] = 'distribuidor inexistente';
                 }
                 break;
-            case 'update':
-                $_POST = $producto->validateForm($_POST);
-                if (!$producto->setId($_POST['idP'])) /*idProducto*/ {
-                    $result['exception'] = 'Producto incorrecto';
-                } elseif (!$producto->setEstadoProducto($_POST['estadoP'])) {
-                    $result['exception'] = 'Estado incorrecto';
-                } elseif ($producto->updateRow()) {
+                 case 'updateStatus':
+                if (!$administrar_distribuidor->setId($_POST['idP'])) {
+                    $result['exception'] = 'Ha ocurrido un error al ejecutar la acción';
+                } elseif (!$administrar_distribuidor->getStatus()) {
+                    $result['exception'] = 'Ha ocurrido un error al obtener el estado';
+                } elseif ($administrar_distribuidor->changeStatus()) {
                     $result['status'] = 1;
-                    $result['message'] = 'Producto modificado correctamente';
-                } else {
+                    $result['message'] = 'Estado cambiado correctamente';
+                } elseif (Database::getException()) {
                     $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'Ha ocurrido un error inseperado';
                 }
                 break;
-            default:
-                $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
         // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
         header('content-type: application/json; charset=utf-8');
         // Se imprime el resultado en formato JSON y se retorna al controlador.
         print(json_encode($result));
-    } else {
-        print(json_encode('Acceso denegado'));
-    }
+   } else {
+       print(json_encode('Acceso denegado'));
+}
 } else {
     print(json_encode('Recurso no disponible'));
 }
