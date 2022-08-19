ea800638b6520b378039642e031183389ef236d5
10Oliver <oliver.erazo1@gmail.com>
2022-08-19 11:34:29 -0600

    Trabajando en graficas de vendedor

--- controladores/publico/publico_vendedor/estadistica_vendedor.js
+++ controladores/publico/publico_vendedor/estadistica_vendedor.js
@@ -2,16 +2,55 @@
 const API_ESTADISTICA = SERVER + 'public/estadistica_vendedor.php?=actio';
 
 //Método que se ejecutará cuando se carga la página
-document.addEventListener("DOMContentLoaded", function () { 
-    //Se carga el slider en la página
+document.addEventListener("DOMContentLoaded", function () {
+    //Se carga la internacionalización de day.js
+    dayjs.locale("es");
+
+    //Obtiene el componente donde se cargará el slider
     var slider = document.getElementById('sliderTotalPromedio');
 
+    //Se crea la función para obtener la fecha a colocar
+    function timestamp(str) {
+        return new Date(str).getTime();
+    }
+
+    //Se crea el slider y su configuración
     noUiSlider.create(slider, {
-        start: [20, 80],
-        connect: true,
+        //Se define el rango de fechas
         range: {
-            'min': 0,
-            'max': 100
-        }
+            min: timestamp(dayjs().subtract(2, 'month').format('YYYY-MM-DD')),
+            max: timestamp(dayjs().format('YYYY-MM-DD'))
+        },
+         connect: true, //Habilitación de la línea entre las manijas (Estilo)
+
+        // Velocidad y opciones que dispondrá el slider
+        step: 24 * 60 * 60 * 1000,
+
+        // Posiciones iniciales las manijas
+         start: [timestamp(dayjs().subtract(7, 'day').format('YYYY-MM-DD')), timestamp(dayjs().format('YYYY-MM-DD'))],
+
+        // Permite o no los decimales, en este caso solo fechas completas
+        format: wNumb({
+            decimals: 0
+        })
     });
-})
+
+    //Componentes que cargarán las fechas seleccionadas
+    var dateValues = [
+        document.getElementById('fechaInicial'),
+        document.getElementById('fechaFinal')
+    ];
+
+    //Actualización de los componentes cuando se mueven las manijas
+    slider.noUiSlider.on('update', function (values, handle) {
+        dateValues[handle].innerHTML = dayjs(new Date(+values[handle])).format('DD, MMMM, YYYY');
+    });
+});
+
+
+//Función para cargar la gráfica de total y promedios de ventas
+function totalPromedio() { 
+    //Crea una variable de tipo Form donde se guardarán los datos
+    let datos = 
+
+}
