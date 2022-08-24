7f9f7d8709b70fc114bb654b6955044e73ef8ba2
Giovanni Arias <85887831+20200044@users.noreply.github.com>
2022-08-24 11:27:04 -0600

    modelos para graficos

--- api/modelos/administrar_cliente.php
+++ api/modelos/administrar_cliente.php
@@ -7,6 +7,21 @@ class AdministrarCliente extends Validator
 {
     // Declaración de atributos (propiedades).
     private $identificador = null;
+    private $estado = null;
+    private $buscador = null;
+    //Direcciones
+    private $id_direccion = null;
+    private $descripcion_direccion = null;
+    private $punto_referencia = null;
+    private $coordenadas = null;
+    private $id_municipio = null;
+    //Grafico
+    private $total = null;
+    private $nombre = null;
+    private $apellido = null;
+    private $correo = null;
+    private $dui = null;
+
 
 
     /*
@@ -23,6 +38,204 @@ class AdministrarCliente extends Validator
         }
     }
 
+    public function setEstado($value)
+    {
+        if ($this->validateBoolean($value)) {
+            $this->estado = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setBuscador($value)
+    {
+        if ($this->validateString($value, 0, 50)) {
+            $this->buscador = '%' . $value . '%';
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setDescripcionDireccion($valor)
+    {
+        if ($valor) {
+            if ($this->validateString($valor, 1, 250)) {
+                $this->descripcion_direccion = $valor;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            $this->descripcion_direccion = null;
+            return true;
+        }
+    }
+
+    public function setPuntoReferencia($valor)
+    {
+        if ($valor) {
+            if ($this->validateString($valor, 1, 50)) {
+                $this->punto_referencia = $valor;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            $this->punto_referencia = null;
+            return true;
+        }
+    }
+
+    public function setCoordenadas($valor)
+    {
+        if ($valor) {
+            if ($this->validateString($valor, 1, 250)) {
+                $this->coordenadas = $valor;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            $this->coordenadas = null;
+            return true;
+        }
+    }
+
+    public function setIdMunicipio($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id_municipio = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setIdDireccion($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id_direccion = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setTotal($value)
+    {
+        if ($this->validateMoney($value)) {
+            $this->total = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setNombre($value)
+    {
+        if ($this->validateAlphanumeric($value, 1, 50)) {
+            $this->nombre = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setApellido($value)
+    {
+        if ($this->validateAlphanumeric($value, 1, 50)) {
+            $this->apellido = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setCorreo($value)
+    {
+        if ($this->validateEmail($value)) {
+            $this->correo = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setDUI($value)
+    {
+        if ($this->validateDUI($value)) {
+            $this->dui = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    *   Métodos para obtener valores de los atributos.
+    */
+    public function getIdentificador()
+    {
+        return $this->identificador;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function getDescripcionDireccion()
+    {
+        return $this->descripcion_direccion;
+    }
+
+    public function getPuntoReferencia()
+    {
+        return $this->punto_referencia;
+    }
+
+    public function getCoordenadas()
+    {
+        return $this->coordenadas;
+    }
+
+    public function getIdMunicipio()
+    {
+        return $this->id_municipio;
+    }
+
+    public function getIdDireccion()
+    {
+        return $this->id_direccion;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    public function getDUI()
+    {
+        return $this->dui;
+    }
+
     /*
     *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
     */
@@ -31,10 +244,108 @@ class AdministrarCliente extends Validator
 
     public function obtenerClientes()
     {
-        $sql = "SELECT id_cliente, CONCAT(nombre_cliente,' ', apellido_cliente) AS nombre_completo, dui_cliente, correo_cliente, usuario_cliente, status_cliente, fecha_registro_cliente FROM cliente
-        WHERE status_cliente NOT IN (false)";
+        $sql = "SELECT id_cliente, CONCAT(nombre_cliente,' ', apellido_cliente) AS nombre_completo,
+         dui_cliente, correo_cliente, usuario_cliente, status_cliente, fecha_registro_cliente, telefono_cliente FROM cliente
+         ORDER BY id_cliente";
         $params = null;
         return Database::getRows($sql, $params);
+    }
 
+    //función para cambiar el estado de los clientes
+
+    public function cambiarEstado()
+    {
+        $sql = "UPDATE cliente SET status_cliente = ? WHERE id_cliente = ?";
+        $params = array($this->estado, $this->identificador);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function buscar()
+    {
+        $sql = "SELECT id_cliente, CONCAT(nombre_cliente,' ', apellido_cliente) AS nombre_completo,
+         dui_cliente, correo_cliente, usuario_cliente, status_cliente, fecha_registro_cliente, telefono_cliente FROM cliente
+         WHERE CONCAT(nombre_cliente,' ', apellido_cliente) ILIKE ? OR correo_cliente ILIKE ? OR usuario_cliente ILIKE ?
+         ORDER BY id_cliente";
+        $params = array($this->buscador, $this->buscador, $this->buscador);
+        return Database::getRows($sql, $params);
+    }
+
+    //Para las direcciones de cliente//
+
+    //Leer direcciones de cliente logueado
+    public function readDirecciones()
+    {
+        $sql = "SELECT  id_direccion, descripcion_direccion, punto_referencia,nombre_municipio
+		from direccion
+		inner join cliente using (id_cliente)
+		inner join municipio using (id_municipio)
+		where id_cliente = ?";
+        $params = array(2);/*SESSION[id_cliente]*/
+        return Database::getRows($sql, $params);
+    }
+
+    //Leer municipios
+    public function readMunicipios()
+    {
+        $sql = "SELECT id_municipio, nombre_municipio, id_departamento
+         from municipio
+         order by nombre_municipio";
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    //Leer departamentos
+    public function readDepartamentos()
+    {
+        $sql = "SELECT id_departamento, nombre_departamento
+         from departamento";
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    // Insertar direccion
+    public function createDireccion()
+    {
+        $sql = 'INSERT INTO direccion(descripcion_direccion,punto_referencia,coordenadas,id_cliente,id_municipio) values (?,?,?,?,?)';
+        $params = array($this->descripcion_direccion, $this->punto_referencia, $this->coordenadas, 2, $this->id_municipio);
+        return Database::executeRow($sql, $params);
+    }
+
+    //Leemos dato individual
+    public function readOneDireccion()
+    {
+        $sql = "SELECT id_direccion,descripcion_direccion,punto_referencia,coordenadas, (CAST(split_part(coordenadas,',',1) AS VARCHAR))as latitude,(CAST(split_part(coordenadas,', ',2) AS VARCHAR))as longitud,id_cliente,id_municipio
+                FROM direccion 
+                WHERE id_direccion = ?";
+        $params = array($this->id_direccion);
+        return Database::getRow($sql, $params);
+    }
+
+    //Actualizamos direccion
+    public function updateDireccion()
+    {
+        $sql = 'UPDATE direccion SET descripcion_direccion=?,punto_referencia=?,coordenadas=?,id_municipio=? where id_direccion=?';
+        $params = array($this->descripcion_direccion, $this->punto_referencia, $this->coordenadas, $this->id_municipio, $this->id_direccion);
+        return Database::executeRow($sql, $params);
+    }
+
+    //Eliminamos direccion
+    public function deleteDireccion()
+    {
+        $sql = 'DELETE from direccion  where id_direccion = ?';
+        $params = array($this->id_direccion);
+        return Database::executeRow($sql, $params);
+    }
+
+    //Grafico Top 5 clientes mas destacados, valor ventas en dinero
+    public function topClientesDestacados()
+    {
+        $sql = "SELECT total, CONCAT(nombre_cliente,' ', apellido_cliente) as nombre_cliente
+        FROM factura
+        INNER JOIN direccion ON factura.id_direccion = direccion.id_direccion
+        INNER JOIN cliente ON direccion.id_cliente = cliente.id_cliente
+        ORDER BY total desc limit 5";
+        $params = null;/*SESSION[id_cliente]*/
+        return Database::getRows($sql, $params);
     }
 }
--- api/modelos/administrar_productos.php
+++ api/modelos/administrar_productos.php
@@ -16,9 +16,12 @@ class Producto extends Validator
     private $nombre_vendedor = null;
     private $marca_producto = null;
     private $estado_producto = null;
-
-
-
+    //Para cliente
+    private $id_detalle_producto = null;
+    private $buscadorI = null;
+    private $buscadorL = null;
+    private $id_categoria = null;
+  
     /*
     *   Métodos para validar y asignar valores de los atributos.
     */
@@ -147,6 +150,46 @@ class Producto extends Validator
         }
     }
 
+    public function setIdDetalleProducto($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id_detalle_producto = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setBuscadorI($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->buscadorI = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setBuscadorL($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->buscadorL = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setIdCategoria($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id_categoria = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /*
     *   Métodos para obtener valores de los atributos.
     */
@@ -200,19 +243,30 @@ class Producto extends Validator
         return $this->estado_producto;
     }
 
+    public function getIdDetalleProducto()
+    {
+        return $this->id_detalle_producto;
+    }
+
+    public function getIdCategoria()
+    {
+        return $this->id_categoria;
+    }
+
+
 
     /*
     *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
     */
     public function searchRows($value)
     {
-        $sql = "SELECT id_producto, nombre_producto, cantidad_producto, descripcion_producto, precio_producto, imagen, categoria.categoria,CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca, estado_producto
+        $sql = "SELECT id_producto, nombre_producto, cantidad_producto, descripcion_producto, precio_producto, imagen, categoria.categoria,CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca, status_producto
 		from producto producto
 		inner join categoria categoria on producto.id_categoria = categoria.id_categoria_producto
 		inner join vendedor using (id_vendedor)
 		inner join marca using (id_marca)
         where nombre_producto ILIKE ? or categoria ILIKE ? or nombre_vendedor ILIKE ? or nombre_marca ILIKE ? ";
-        $params = array("%$value%","%$value%","%$value%","%$value%");
+        $params = array("%$value%", "%$value%", "%$value%", "%$value%");
         return Database::getRows($sql, $params);
     }
 
@@ -220,7 +274,7 @@ class Producto extends Validator
     //Función para leer todos los datos
     public function readAll()
     {
-        $sql = "SELECT id_producto, nombre_producto, cantidad_producto, descripcion_producto, precio_producto, imagen, categoria.categoria,CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca, estado_producto
+        $sql = "SELECT id_producto, nombre_producto, cantidad_producto, descripcion_producto, precio_producto, imagen, categoria.categoria,categoria.imagen_categoria,CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca, status_producto
 		from producto producto
 		inner join categoria categoria on producto.id_categoria = categoria.id_categoria_producto
 		inner join vendedor using (id_vendedor)
@@ -233,7 +287,7 @@ class Producto extends Validator
     //Funcion para leer producto individual
     public function readOne()
     {
-        $sql = 'SELECT id_producto, estado_producto
+        $sql = 'SELECT id_producto, status_producto
                 FROM producto
                 WHERE id_producto = ?';
         $params = array($this->id);
@@ -242,12 +296,152 @@ class Producto extends Validator
 
     public function updateRow()
     {
-        
+
         $sql = 'UPDATE producto
-                SET estado_producto=?
+                SET status_producto=?
                 WHERE id_producto = ?';
-        $params = array($this->estado_producto,$this->id);
+        $params = array($this->estado_producto, $this->id);
         return Database::executeRow($sql, $params);
     }
-    
+
+    /////////////////Cliente//////////////////////////
+    //Leemos productos 
+    public function readProductos()
+    {
+        $sql = "SELECT producto.id_producto, nombre_producto, cantidad_producto, descripcion_producto, precio_producto, imagen,CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca, status_producto,avg(comentario_producto.valoracion) as calidad,comentario_producto.id_detalle
+		from comentario_producto comentario_producto
+		FULL OUTER join detalle_factura as detalle_factura on comentario_producto.id_detalle = detalle_factura.id_detalle
+		FULL OUTER join producto as producto on detalle_factura.id_producto = producto.id_producto
+		FULL OUTER join categoria as categoria on producto.id_categoria = categoria.id_categoria_producto
+		inner join vendedor using (id_vendedor)
+		inner join marca using (id_marca)
+        where status_producto = true
+        group by producto.id_producto,vendedor.nombre_vendedor,vendedor.apellido_vendedor,marca.nombre_marca, comentario_producto.id_detalle";
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    //Leemos categorias
+    public function   readCategoria()
+    {
+        $sql = "SELECT id_categoria_producto, categoria, imagen_categoria
+		from categoria
+        where status_categoria = true
+        order by categoria";
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    //Función para leer calidad de los productos(No se ocupa )
+    public function readCali()
+    {
+        $sql = "SELECT avg(valoracion) as calidad, comentario_producto.id_detalle 
+        from comentario_producto comentario_producto
+		FULL OUTER join detalle_factura as detalle_factura on comentario_producto.id_detalle = detalle_factura.id_detalle
+		FULL OUTER join producto as producto on detalle_factura.id_producto = producto.id_producto
+		FULL OUTER join categoria as categoria on producto.id_categoria = categoria.id_categoria_producto
+		inner join vendedor using (id_vendedor)
+		inner join marca using (id_marca)
+        where status_producto = true and comentario_producto.id_detalle = ?
+        group by comentario_producto.id_detalle";
+        $params = array($this->id_detalle_producto);
+        return Database::getRows($sql, $params);
+    }
+
+    //Leemos comentarios de productos
+    public function readComent()
+    {
+        $sql = "SELECT comentario, cliente.usuario_cliente, valoracion as calidad
+        from comentario_producto comentario_producto
+		FULL OUTER join detalle_factura as detalle_factura on comentario_producto.id_detalle = detalle_factura.id_detalle
+		FULL OUTER join producto as producto on detalle_factura.id_producto = producto.id_producto
+		FULL OUTER join categoria as categoria on producto.id_categoria = categoria.id_categoria_producto
+		FULL OUTER join factura as factura on detalle_factura.id_factura = factura.id_factura
+		INNER JOIN direccion as direccion on factura.id_direccion = direccion.id_direccion
+		INNER JOIN cliente as cliente on direccion.id_cliente  = cliente.id_cliente
+		inner join vendedor using (id_vendedor)
+		inner join marca using (id_marca)
+        where status_producto = true and producto.id_producto = ?
+        group by comentario, cliente.usuario_cliente, valoracion";
+        $params = array($this->id);
+        return Database::getRows($sql, $params);
+    }
+
+    //Leemos productos por categoria
+    public function readProductosCategoria()
+    {
+        $sql = "SELECT producto.id_producto, nombre_producto, cantidad_producto, descripcion_producto, precio_producto, imagen,CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca, status_producto,avg(comentario_producto.valoracion) as calidad,comentario_producto.id_detalle
+		from comentario_producto comentario_producto
+		FULL OUTER join detalle_factura as detalle_factura on comentario_producto.id_detalle = detalle_factura.id_detalle
+		FULL OUTER join producto as producto on detalle_factura.id_producto = producto.id_producto
+		FULL OUTER join categoria as categoria on producto.id_categoria = categoria.id_categoria_producto
+		inner join vendedor using (id_vendedor)
+		inner join marca using (id_marca)
+        where status_producto = true and id_categoria = ?
+        group by producto.id_producto,vendedor.nombre_vendedor,vendedor.apellido_vendedor,marca.nombre_marca,comentario_producto.id_detalle";
+        $params = array($this ->id_categoria);
+        return Database::getRows($sql, $params);
+    }
+
+    //Buscador por precio
+    public function searchProductoPrecio()
+    {
+        $sql = "SELECT producto.id_producto, nombre_producto, cantidad_producto, descripcion_producto, precio_producto, imagen,CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca, status_producto,avg(comentario_producto.valoracion) as calidad
+		from comentario_producto comentario_producto
+		FULL OUTER join detalle_factura as detalle_factura on comentario_producto.id_detalle = detalle_factura.id_detalle
+		FULL OUTER join producto as producto on detalle_factura.id_producto = producto.id_producto
+		FULL OUTER join categoria as categoria on producto.id_categoria = categoria.id_categoria_producto
+		inner join vendedor using (id_vendedor)
+		inner join marca using (id_marca)
+        where status_producto = true and (precio_producto >= ? and precio_producto <= ?)
+        group by producto.id_producto,vendedor.nombre_vendedor,vendedor.apellido_vendedor,marca.nombre_marca ";
+        $params = array($this->buscadorI,$this->buscadorL);
+        return Database::getRows($sql, $params);
+    }
+
+    //Buscador por calidad
+    public function searchProductoCalidad($valorU,$valorD,$valorT,$valorC,$valorCi)
+    {
+        $sql = "SELECT producto.id_producto, nombre_producto, cantidad_producto, descripcion_producto, precio_producto, imagen,CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca, status_producto,avg(comentario_producto.valoracion) as calidad
+		from comentario_producto comentario_producto
+		FULL OUTER join detalle_factura as detalle_factura on comentario_producto.id_detalle = detalle_factura.id_detalle
+		FULL OUTER join producto as producto on detalle_factura.id_producto = producto.id_producto
+		FULL OUTER join categoria as categoria on producto.id_categoria = categoria.id_categoria_producto
+		inner join vendedor using (id_vendedor)
+		inner join marca using (id_marca)
+        where status_producto = true and valoracion = ? or valoracion = ? or valoracion = ? or valoracion = ? or valoracion = ? 
+        group by producto.id_producto,vendedor.nombre_vendedor,vendedor.apellido_vendedor,marca.nombre_marca ";
+        $params = array($valorU,$valorD,$valorT,$valorC,$valorCi);
+        return Database::getRows($sql, $params);
+    }
+
+    public function readOneProducto()
+    {
+        $sql = "SELECT id_producto, nombre_producto,cantidad_producto, descripcion_producto,precio_producto, imagen,status_producto, categoria, CONCAT(nombre_vendedor,' ',apellido_vendedor) as nombre_vendedor, nombre_marca
+                FROM producto producto
+                INNER JOIN vendedor using (id_vendedor)
+                INNER JOIN marca using (id_marca)
+                INNER JOIN categoria as categoria on producto.id_categoria = categoria.id_categoria_producto
+                WHERE id_producto = ?" ;
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    //Grafico Porcentaje de productos por marca
+    public function porcentajeProductos()
+    {
+        $sql = "SELECT categoria, SUM(ROUND((cantidad_producto * 100.0 /
+            (SELECT sum(cantidad_producto)
+            FROM producto
+            )
+        ), 2))
+        porcentaje
+        FROM detalle_factura
+        INNER JOIN factura ON detalle_factura.id_factura = factura.id_factura
+        INNER JOIN producto ON detalle_factura.id_producto = producto.id_producto
+        INNER JOIN categoria ON producto.id_categoria = categoria.id_categoria
+        GROUP BY categoria;";
+        $params = null;/*SESSION[id_cliente]*/
+        return Database::getRows($sql, $params);
+    }
 }
