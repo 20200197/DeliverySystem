de8ae8240445d3fdb10160a057a3f728f467af80
RodriB17 <20170373@ricaldone.edu.sv>
2022-09-03 09:46:39 -0600

    Arreglos en la vista de y navegabilidad

--- api/publico/historial_repartidor.php
+++ api/publico/historial_repartidor.php
@@ -12,30 +12,30 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'exception' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    //if (isset($_SESSION['id_admin'])) {
-    $result['session'] = 1;
-    // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
-    switch ($_GET['action']) {
-            //Leer direcciones del cliente  
-        case 'readAll':
-            if ($result['dataset'] = $hist->readHistorialRepartidor()) {
-                $result['status'] = 1;
-            } elseif (Database::getException()) {
-                $result['exception'] = Database::getException();
-            } else {
-                $result['exception'] = 'No hay datos registrados';
-            }
-            break;
-        default:
-            $result['exception'] = 'Acción no disponible dentro de la sesión';
+    if (isset($_SESSION['id_repartidor'])) {
+        $result['session'] = 1;
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+                //Leer direcciones del cliente  
+            case 'readAll':
+                if ($result['dataset'] = $hist->readHistorialRepartidor()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay datos registrados';
+                }
+                break;
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
+        }
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+    } else {
+        print(json_encode('Acceso denegado'));
     }
-    // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
-    header('content-type: application/json; charset=utf-8');
-    // Se imprime el resultado en formato JSON y se retorna al controlador.
-    print(json_encode($result));
-    // } else {
-    //   print(json_encode('Acceso denegado'));
-    // }
 } else {
     print(json_encode('Recurso no disponible'));
 }
--- controladores/publico/publico_repartidor/index.js
+++ controladores/publico/publico_repartidor/index.js
@@ -20,3 +20,21 @@ document.getElementById('login-form').addEventListener('submit', function () {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch(API_REPARTIDOR + 'session', {
+        method: 'get'
+    }).then(function (request) {
+        if (request.ok) {
+            request.json().then(function (response) {
+                if (response.session) {
+                    sweetAlert(1, response.message, 'historial.html');
+                } else {
+                    sweetAlert(2, response.exception, null);
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    });
+});
