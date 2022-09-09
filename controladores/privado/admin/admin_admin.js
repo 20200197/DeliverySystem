deb75093e3b60c22d23c38434e64e0950b5106fb
RodriB17 <20170373@ricaldone.edu.sv>
2022-09-09 17:59:03 -0600

    revert comentarios

--- controladores/privado/admin/admin_admin.js
+++ controladores/privado/admin/admin_admin.js
@@ -76,7 +76,7 @@ document.getElementById('save-form').addEventListener('submit', function () {
         if (request.ok) {
             request.json().then(function (response) {
                 if (response.status) {
-                    sweetAlert(1, response.message, null);
+                    sweetAlert(1, response.message, 'admin_admin.html');
                 } else {
                     sweetAlert(2, response.exception, null);
                 }
