a2312a847a1781fc81ca7950af29f78bc4522f2b
20200197 <20200197@ricaldone.edu.sv>
2022-09-10 16:00:03 -0600

    Update database.php

--- api/ayudantes/database.php
+++ api/ayudantes/database.php
@@ -16,7 +16,7 @@ class Database
     {
         // Credenciales para establecer la conexión con la base de datos.
         $server = 'localhost';
-        $database = 'dbdeliverysystem';
+        $database = 'dbdeliverysystemmmm';
         $username = 'postgres';
         $password = 'Academica20_21';
 
