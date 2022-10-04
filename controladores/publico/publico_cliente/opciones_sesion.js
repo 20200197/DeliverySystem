1dd54c30f2950ed17f245c8ca67205fc0c4b675a
10Oliver <oliver.erazo1@gmail.com>
2022-10-04 14:10:14 -0600

    Nuevo diseño de sidebar y narva para la sesión en cliente terminado

--- controladores/publico/publico_cliente/opciones_sesion.js
+++ controladores/publico/publico_cliente/opciones_sesion.js
@@ -3,7 +3,7 @@ const API_perfil = SERVER + "publico/sesion.php?action=";
 
 //Método que se ejecuta cuando se carga la página
 document.addEventListener("DOMContentLoaded", function () {
-    M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+    M.Tooltip.init(document.querySelectorAll(".tooltipped"));
     //Se busca si se ha iniciado sesión o no
     fetch(API_perfil + "obtenerSesionC", {
         method: "get",
@@ -15,20 +15,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 //Se crea la variable donde se guardarán los datos
                 let data = [];
                 //se crea la variable donde se guardará el HTML a inyectar
-                let contenido = [];
+                let contenido = [],
+                    contenido2 = [];
                 // Se comprueba si la respuesta es satisfactoria para obtener los datos, de lo contrario se muestra un mensaje con la excepción.
                 if (response.status) {
                     data = response.dataset;
                     //Si hay sesión se reeemplazan las opciones
                     contenido = `
-                    <div class="col l11 m8 s12 right-align">
+                    <div class="col l11 m10 s9 right-align">
+                       
                         <span class="black-text">${data.nombre}</span>
                     </div>
-                    <div class="col l1 m4 s12 left">
-                        <a href="perfil_cliente.html"><i class="icono-sesion material-icons black-text">account_circle</i></a>
+                    <div class="col l1 m2 s3 right valign-wrapper">
+                        <a href="perfil.html">
+                        <img src='../../../api/imagenes/repartidor/foto_repartidor/${data.foto_cliente}' class="foto-perfil-repartidor-grande"></a>
                     </div>`;
+                    contenido2 = `
+                    <a href="perfil.html">
+                                       <img src='../../../api/imagenes/repartidor/foto_repartidor/${data.foto_cliente}' class="foto-perfil-repartidor">
+                    <span class="white-text hide-on-large-only ">${data.nombre}</span></a>
+                   `;
                     //se incrustan en el html
                     document.getElementById("opciones_navbar").innerHTML = contenido;
+                    document.getElementById("sesion_navbar").innerHTML = contenido2;
                 } else {
                     //si no hay sesión se coloca la opción para iniciar sesión
                     contenido = `
@@ -36,8 +45,12 @@ document.addEventListener("DOMContentLoaded", function () {
                         <a href="login.html" class="waves-effect waves-light btn blue accent-2 boton_iniciar_sesion">Iniciar sesion</a>
                     </div>
                     `;
+                    contenido2 = `
+                         <a href="login.html" class="waves-effect waves-light btn blue accent-2 boton_iniciar_sesion">Iniciar sesion</a>
+                   `;
                     //se incrustan en el html
                     document.getElementById("opciones_navbar").innerHTML = contenido;
+                    document.getElementById("sesion_navbar").innerHTML = contenido2;
                 }
                 // Se envían los datos a la función del controlador para llenar la tabla en la vista.
             });
@@ -47,4 +60,3 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
-
